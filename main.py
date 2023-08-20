b14280bb8bc3ceccf8509a0be52b32683ae4bf56
Giuseppe Frattura <giuseppefrattura@gmail.com>
2023-08-20 12:03:34 +0200

    Update main.py
    updated service port

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ def configure_routing():
 
 if __name__ == '__main__':
     configure()
-    uvicorn.run(api, port=8000, host='127.0.0.1')
+    uvicorn.run(api, port=8010, host='127.0.0.1')
 else:
     configure()
